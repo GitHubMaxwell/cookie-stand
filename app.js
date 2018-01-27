10216bae5d277dc53d13450fc908a86835686bdf
GitHubMaxwell <maxwell.rediker@gmail.com>
2018-01-27 09:48:16 -0800

    finished lab6 object, now to organize them horizontally

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 'use strict';
 
-//going to need an array of objects so you can iterate through them
+//going to need an global array of times so you can iterate through them in all the below objects
 var opHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+/////////////////////////// first object for first store /////////////////////////
 var storeOne = {
   location: '1st and Pike',
   minCustomer: 23,
@@ -10,25 +12,18 @@ var storeOne = {
   randCust: [],
   cookiesSold: [],
 
-  //the random math generator should calculate the random number passing in with the min and max customers as the limits, take that number and times it by  and return the 
   hourlyTotal: function() {
-    //you do want the random math generator in the for loop so it makes a new one each iteration, also defining the totalCookies in the
     for (var i = 0; i < opHours.length; i++) {
       this.randCust.push(Math.floor(Math.random() * (this.maxCustomer - this.minCustomer)) + this.minCustomer);
-      //console.log(this.hourTotalArray);
-      //var cookieAmount = (rand * avgSaleRate);
       this.cookiesSold.push(Math.round(this.avgSale * this.randCust[i]));
     }
   },
   render: function () {
     this.hourlyTotal();
     var location = document.getElementById('location1');
-    //why do i have to create the element (thats empty and doesnt have any text or content in it) if its already defined in the html. maybe i dont and all i have to do is target it and fill in the text with
-    var h2El = document.createElement('h2');
-    //then you add the content that is defined here in the object
-    h2El.textContent = this.location;
-    //then you have to take 
-    location.appendChild(h2El);
+    var h3El = document.createElement('h3');
+    h3El.textContent = this.location;
+    location.appendChild(h3El);
     for (var j = 0; j < opHours.length; j++){
       var list = document.getElementById('firstandpike');
       var liEl = document.createElement('li');
@@ -38,9 +33,8 @@ var storeOne = {
   },
 };
 storeOne.render();
-console.log(storeOne.randCust);
-console.log(storeOne.cookiesSold);
 
+/////////////////////////// second object for second store /////////////////////////
 var storeTwo = {
   location: 'SeaTac Airport',
   minCustomer: 3,
@@ -48,74 +42,122 @@ var storeTwo = {
   avgSale: 1.2,
   randCust: [],
   cookiesSold: [],
-  //get a random number of customers push to array
-  //times the avgSale. push to array
+
   hourlyTotal: function() {
-    for (var i = 0; i < length.opHours; i++) {
+    for (var i = 0; i < opHours.length; i++) {
       this.randCust.push(Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
       this.cookiesSold.push(Math.round(this.avgSale * this.randCust[i]));
     }
   },
   render: function() {
-    //you have to locate the element that is the title of the list. it doesn have to be in the for loop because its just a title theres no need to iterate it
-    //you have to store the location of the element by its id in a variable,
+    this.hourlyTotal();
     var location = document.getElementById('location2');
-    //then you have to
-    var h3El = document.textContent;
-    //you then have to declare a variable to store the textContent that you wish to put in the title in a variable
-    h3El.textContent = storeTwo.location;
+    var h3El = document.createElement('h3');
+    h3El.textContent = this.location;
     location.appendChild(h3El);
-    for(var j = 0; j < length.opHours; j++) {
-      //call other methods and build the list
+
+    for(var j = 0; j < opHours.length; j++) {
+      var list = document.getElementById('seaTac');
+      var liEl = document.createElement('li');
+      liEl.textContent = (opHours[j] + ': ' + this.cookiesSold[j]);
+      list.appendChild(liEl);
     }
   },
 };
 storeTwo.render();
-/*
+
+/////////////////////////// third object for third store /////////////////////////
 var storeThree = {
   location: 'Seattle Center',
   minCustomer: 11,
   maxCustomer: 38,
   avgSale: 3.7,
+  randCust: [],
+  cookiesSold: [],
 
   hourlyTotal: function() {
-    console.log('My name is ' + storeOne.name + ' and I like to roam around in the ' + storeOne.territory + '.');
-  }
+    for (var i = 0; i < opHours.length; i++) {
+      this.randCust.push(Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
+      this.cookiesSold.push(Math.round(this.avgSale * this.randCust[i]));
+    }
+  },
+  render: function() {
+    this.hourlyTotal();
+    var location = document.getElementById('location3');
+    var h3El = document.createElement('h3');
+    h3El.textContent = this.location;
+    location.appendChild(h3El);
+
+    for(var j = 0; j < opHours.length; j++) {
+      var list = document.getElementById('seaCenter');
+      var liEl = document.createElement('li');
+      liEl.textContent = (opHours[j] + ': ' + this.cookiesSold[j]);
+      list.appendChild(liEl);
+    }
+  },
 };
+storeThree.render();
+
+/////////////////////////// fourth object for fourth store /////////////////////////
 var storeFour = {
   location: 'Capitol Hill',
   minCustomer: 20,
   maxCustomer: 38,
   avgSale: 2.3,
+  randCust: [],
+  cookiesSold: [],
 
   hourlyTotal: function() {
-    console.log('My name is ' + storeOne.name + ' and I like to roam around in the ' + storeOne.territory + '.');
-  }
+    for (var i = 0; i < opHours.length; i++) {
+      this.randCust.push(Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
+      this.cookiesSold.push(Math.round(this.avgSale * this.randCust[i]));
+    }
+  },
+  render: function() {
+    this.hourlyTotal();
+    var location = document.getElementById('location4');
+    var h3El = document.createElement('h3');
+    h3El.textContent = this.location;
+    location.appendChild(h3El);
+
+    for(var j = 0; j < opHours.length; j++) {
+      var list = document.getElementById('capHill');
+      var liEl = document.createElement('li');
+      liEl.textContent = (opHours[j] + ': ' + this.cookiesSold[j]);
+      list.appendChild(liEl);
+    }
+  },
 };
+storeFour.render();
+
+/////////////////////////// fifth object for fifth store /////////////////////////
 var storeFive = {
   location: 'Alki',
   minCustomer: 2,
   maxCustomer: 16,
   avgSale: 4.6,
+  randCust: [],
+  cookiesSold: [],
 
   hourlyTotal: function() {
-    console.log('My name is ' + storeOne.name + ' and I like to roam around in the ' + storeOne.territory + '.');
-  }
-};
-*/
-/*
-var storeLoc = document.getElementByClass('location');
-var minimumCust = document.getElementByClass('minCust');
-var maximumCust = document.getElementByClass('maxCust');
-var avgSale = document.getElementByClass('avgCookieSale');
-*/
-
-/*
-hourlyTotal: function(maxCustomer, minCustomer, avgSaleRate) {
-    for (var i = 0; i < 15; i++) {
-      var totalCookies = (Math.random() * (maxCustomer - minCustomer) + minCustomer) * avgSaleRate;
+    for (var i = 0; i < opHours.length; i++) {
+      this.randCust.push(Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer));
+      this.cookiesSold.push(Math.round(this.avgSale * this.randCust[i]));
     }
-    return totalCookies;
-  }
+  },
+  render: function() {
+    this.hourlyTotal();
+    var location = document.getElementById('location5');
+    var h3El = document.createElement('h3');
+    h3El.textContent = this.location;
+    location.appendChild(h3El);
+
+    for(var j = 0; j < opHours.length; j++) {
+      var list = document.getElementById('alki');
+      var liEl = document.createElement('li');
+      liEl.textContent = (opHours[j] + ': ' + this.cookiesSold[j]);
+      list.appendChild(liEl);
+    }
+  },
 };
-*/
+storeFive.render();
